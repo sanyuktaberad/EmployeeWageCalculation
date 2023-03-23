48752814ce2e72c8bf9d28d431e964186f29c3f9
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-23 14:00:33 +0530

    Add: Code for daily wage Calculation is added.

--- src/employee_Wage_Calculation/EmployeeWageCalculation.java
+++ src/employee_Wage_Calculation/EmployeeWageCalculation.java
@@ -1,14 +1,18 @@
 package employee_Wage_Calculation;
 
 public class EmployeeWageCalculation {
+	static int wagePerHour = 20;
+	static int workingHoursPerDay = 8;
+	static int wage = (int)(wagePerHour * workingHoursPerDay);
+	
 	public static void main(String[] args) {
 		
 		double number = Math.floor(Math.random() * 10) % 2;
 		
 		if (number == 1) {
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present & its daily wage is " + wage);
 		} else {
-			System.out.println("Employee is Absent");
+			System.out.println("Employee is Absent & its daily wage is 0");
 		}
 	}
 
