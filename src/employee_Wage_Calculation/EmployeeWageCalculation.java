ac346c9bb8f1df1a95478cd99bf92fa37e38c0ec
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-23 20:32:44 +0530

    Add: Code by using switch Statwment is added.

--- src/employee_Wage_Calculation/EmployeeWageCalculation.java
+++ src/employee_Wage_Calculation/EmployeeWageCalculation.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class EmployeeWageCalculation {
 	static int wagePerHour = 20;
-	static int workingHoursPerDay = 8;
+	static int workingHoursFullTimerPerDay = 8;
 	static int workingHoursForPartTimerPerDay = 4;
-	static int wage = (int) (wagePerHour * workingHoursPerDay);
-	static int wageForPartTimer = (int) (wagePerHour * workingHoursForPartTimerPerDay);
+	static int wage = (int)(wagePerHour * workingHoursFullTimerPerDay);
+	static int wage2 = (int)(wagePerHour * workingHoursForPartTimerPerDay);
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -16,14 +16,21 @@ public class EmployeeWageCalculation {
 				"Enter 1 if the employee is present & fulltimer; enter 2 if the employee is present & parttimer; enter 0 if the employee is absent. ");
 		int presenty = sc.nextInt();
 
-		if (presenty == 1) {
-			System.out.println("Employee is Present & its daily wage is " + wage);
-		} else if (presenty == 2) {
-			System.out.println("Employee is Present & its daily wage is " + wageForPartTimer);
-		} else {
-			System.out.println("Employee is Absent & its daily wage is 0");
+		int wage = (int) (wagePerHour * workingHoursForPartTimerPerDay);
+		int wage2 = (int) (wagePerHour * workingHoursForPartTimerPerDay);
+
+		switch (presenty) {
+		case 1:
+			System.out.println("Employee is Present & Fulltimer hence its monthly wage is : " + wage + ".");
+			break;
+
+		case 2:
+			System.out.println("Employee is Present &  Parttimer hence its monthly wage is : " + wage2 + ".");
+			break;
+
+		default:
+			System.out.println("Employee is Absent & its daily wage is 0.");
 
 		}
-
 	}
 }
