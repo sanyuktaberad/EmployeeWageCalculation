f2d85704172c6dbe913a540b6abaebb16d2d6188
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-23 13:44:10 +0530

    Add: Code to check attendence is added.

--- src/employee_Wage_Calculation/EmployeeWageCalculation.java
+++ src/employee_Wage_Calculation/EmployeeWageCalculation.java
@@ -1,5 +1,15 @@
 package employee_Wage_Calculation;
 
 public class EmployeeWageCalculation {
+	public static void main(String[] args) {
+		
+		double number = Math.floor(Math.random() * 10) % 2;
+		
+		if (number == 1) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
+	}
 
 }
