e623f940e17e4257b8fdc25d127421c396b57ab9
sanyuktaberad <sanyukta.berad@gmail.com>
2023-03-31 23:50:41 +0530

    Refactor: Code to write a Class Method to Compute Employee Wage.

--- src/employee_Wage_Calculation/EmployeeWageCalculation.java
+++ src/employee_Wage_Calculation/EmployeeWageCalculation.java
@@ -3,36 +3,48 @@ package employee_Wage_Calculation;
 import java.util.Scanner;
 
 public class EmployeeWageCalculation {
-	static int wagePerHour = 20;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int MAX_HOURS_IN_MONTH = 100;
+	public static final int NUMBER_OF_WORKING_DAYS = 20;
 	
-	
+	public int computeEmployeeWage() {
+		int workingHours=0;
+		int totalWorkingHours = 0;
+		int totalWorkingDays = 0;
+		
+		while (totalWorkingHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+		
+			int employeeStatus = (int) (Math.floor(Math.random() * 10 ) % 3);
+			
+			switch (employeeStatus) {
+			case 1:
+				System.out.println("Employee is Present & Fulltimer.");
+				workingHours = 8;
+				break;
+
+			case 2:
+				System.out.println("Employee is Present &  Parttimer.");
+				workingHours = 4;
+				break;
+
+			default:
+				System.out.println("Employee is Absent.");
+				workingHours = 0;
+
+			}
+			totalWorkingHours = totalWorkingHours + workingHours;
+			System.out.println ("Total working days = "+ totalWorkingDays);
+			System.out.println ("Total working hours = "+ workingHours);
+		}
+		int totalEmployeeWage = totalWorkingHours * WAGE_PER_HOUR;
+		System.out.println ("Total employee wage = "+ totalEmployeeWage );
+		return totalEmployeeWage;
+		
+		}
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println(
-				"Enter 1 if the employee is present & fulltimer; enter 2 if the employee is present & parttimer; enter 0 if the employee is absent. ");
-		int presenty = sc.nextInt();
-		
-		System.out.println("Enter the value of working days of a month:");
-		int workingDaysForAMonth = sc.nextInt();
-		System.out.println("Enter the value of working hours of a month:");
-		int workingHoursForAMonth = sc.nextInt();
-		
-		int wage = (int)(wagePerHour * workingDaysForAMonth * workingHoursForAMonth );
-		int wage2 = (int)(wagePerHour * workingDaysForAMonth * workingHoursForAMonth );
-
-		switch (presenty) {
-		case 1:
-			System.out.println("Employee is Present & Fulltimer hence its monthly wage is : " + wage + ".");
-			break;
-
-		case 2:
-			System.out.println("Employee is Present &  Parttimer hence its monthly wage is : " + wage2 + ".");
-			break;
-
-		default:
-			System.out.println("Employee is Absent & its daily wage is 0.");
-
+		EmployeeWageCalculation employeeWage = new EmployeeWageCalculation();
+		employeeWage.computeEmployeeWage();
 		}
 	}
-}
+	
