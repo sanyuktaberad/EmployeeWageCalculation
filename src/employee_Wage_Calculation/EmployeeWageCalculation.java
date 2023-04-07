21efeaea9f8f0c5da4b177ed1075591093f02405
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-07 22:29:10 +0530

    Refractor: Code for calculating employee wage for multiple companies is added.

--- src/employee_Wage_Calculation/EmployeeWageCalculation.java
+++ src/employee_Wage_Calculation/EmployeeWageCalculation.java
@@ -3,20 +3,24 @@ package employee_Wage_Calculation;
 import java.util.Scanner;
 
 public class EmployeeWageCalculation {
-	public static final int WAGE_PER_HOUR = 20;
 	public static final int MAX_HOURS_IN_MONTH = 100;
 	public static final int NUMBER_OF_WORKING_DAYS = 20;
+	static int wagePerHour = 0;
+	static int totalWorkingHours = 0;
 	
-	public int computeEmployeeWage() {
-		int workingHours=0;
+	public void computeEmployeeWage() {
+		int workingHours = 0;
 		int totalWorkingHours = 0;
 		int totalWorkingDays = 0;
-		
+		int Tata =1;
+		int Birla =2 ;
+		int infosys = 3;
+
 		while (totalWorkingHours <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
 			totalWorkingDays++;
-		
-			int employeeStatus = (int) (Math.floor(Math.random() * 10 ) % 3);
-			
+
+			int employeeStatus = (int) (Math.floor(Math.random() * 10) % 3);
+
 			switch (employeeStatus) {
 			case 1:
 				System.out.println("Employee is Present & Fulltimer.");
@@ -33,18 +37,39 @@ public class EmployeeWageCalculation {
 				workingHours = 0;
 
 			}
+			System.out.println("Enter the company code => ");
+			Scanner sc = new Scanner(System.in);
+			int companyName = sc.nextInt();
+
+			if (companyName == 1) {
+				wagePerHour = 30;
+			} 
+			else if (companyName == 2) {
+				wagePerHour = 35;
+			} 
+			else if (companyName == 3) {
+				wagePerHour = 25;
+			} 
+			else {
+				System.out.println("Invalid Input...... ");
+			}
+			
 			totalWorkingHours = totalWorkingHours + workingHours;
-			System.out.println ("Total working days = "+ totalWorkingDays);
-			System.out.println ("Total working hours = "+ workingHours);
+			System.out.println("Total working days = " + totalWorkingDays);
+			System.out.println("Total working hours = " + workingHours);
 		}
-		int totalEmployeeWage = totalWorkingHours * WAGE_PER_HOUR;
-		System.out.println ("Total employee wage = "+ totalEmployeeWage );
-		return totalEmployeeWage;
+
 		
-		}
+	}
+
 	public static void main(String[] args) {
 		EmployeeWageCalculation employeeWage = new EmployeeWageCalculation();
 		employeeWage.computeEmployeeWage();
-		}
+		
+		
+		int totalEmployeeWage = totalWorkingHours * wagePerHour;
+		System.out.println("Total employee wage = " + totalEmployeeWage);
+		return;
+
 	}
-	
+}
