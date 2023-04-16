e14d720b1283a2b3878fb61b6200732b043d9e54
sanyuktaberad <sanyukta.berad@gmail.com>
2023-04-16 13:36:59 +0530

    Refractor: Code for saving the total wage for each company is added.

--- src/employee_Wage_Calculation/EmployeeWageCalculation.java
+++ src/employee_Wage_Calculation/EmployeeWageCalculation.java
@@ -5,10 +5,25 @@ import java.util.Scanner;
 public class EmployeeWageCalculation {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-
-	public static int computeEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays,
+	
+	public static String company;
+	public static int empRatePerHour;
+	public static int numOfWorkingDays;
+	public static int maxHoursPerMonth;
+	public static int totalEmpWage;
+	
+	public void EmpWageObjects(String company, int empRatePerHour, int numOfWorkingDays,
 			int maxHoursPerMonth) {
+		this.company= company;
+		this.empRatePerHour = empRatePerHour;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.totalEmpWage = totalEmpWage;
+		
+	}
 
+	public static void computeEmployeeWage() {
+		
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 
 		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
@@ -38,15 +53,28 @@ public class EmployeeWageCalculation {
 			System.out.println("Total working hours = " + empHrs);
 		}
 		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for company " + company + " is " + totalEmpWage);
-		return totalEmpWage;
-
+	}
+	@Override
+	public String toString() {
+		return "Total Employee Wage for Company " + company +" is " + totalEmpWage;
 	}
 
 	public static void main(String[] args) {
-		computeEmployeeWage("DMart", 20, 2, 10);
-		computeEmployeeWage("Reliance", 10, 6, 10);
-		computeEmployeeWage("Tata", 30, 6, 10);
-		computeEmployeeWage("Birla", 35, 7, 10);
+		EmpWageObjects dMart = new EmpWageObjects ("DMart", 20, 2, 10);
+		EmpWageObjects reliance =new EmpWageObjects("Reliance", 10, 6, 10);
+		EmpWageObjects tata =new EmpWageObjects("Tata", 30, 6, 10);
+		EmpWageObjects birla =new EmpWageObjects("Birla", 35, 7, 10);
+		
+		dMart.computeEmployeeWage();
+		System.out.println(dMart);
+		
+		reliance.computeEmployeeWage();
+		System.out.println(reliance);
+		
+		tata.computeEmployeeWage();
+		System.out.println(tata);
+		
+		birla.computeEmployeeWage("Birla", 35, 7, 10);
+		System.out.println(birla);
 	}
 }
